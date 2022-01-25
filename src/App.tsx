2f7c356e33dce3746f315266802ee5f3a7e83fa8
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-01-24 23:31:56 -0600

    initial grid setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const numRows = 50;
+const numCols = 50;
+
+const App: React.FC = () => {
+  const [grid, setGrid] = React.useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`,
+      }}
+    >
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div
+            key={`${i}-${k}`}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[i][k] ? 'pink' : undefined,
+              border: 'solid 1px black',
+            }}
+          />
+        ))
+      )}
     </div>
   );
-}
+};
 
 export default App;
